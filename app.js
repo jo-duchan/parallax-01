d0da1cbb1738c78b716678a8ee19bca602ff3471
jo-duchan <jo_duchan@icloud.com>
2022-12-20 18:10:03 +0900

    init, onScroll, onResize

---
+++ app.js
@@ -0,0 +1,38 @@
+let imageArr;
+let scrollTop;
+let totalNum;
+let percent;
+let totalH;
+let screenH;
+
+function init() {
+  imageArr = document.querySelectorAll(".layout");
+  console.log(imageArr);
+  totalNum = imageArr.length;
+
+  onResize();
+
+  window.addEventListener("scroll", onScroll);
+  window.addEventListener("resize", onResize);
+}
+
+function onScroll() {
+  scrollTop = this.scrollY;
+  percent = Math.min(Math.max((scrollTop / (totalH - screenH)) * 100, 0), 100);
+
+  for (let i = 0; i < totalNum; i++) {
+    imageArr[i].style.transform = `perspective(400px) translate3d(0, 0, ${
+      scrollTop / (5 * (totalNum - i))
+    }px)`;
+  }
+}
+
+function onResize() {
+  totalH = document.body.offsetHeight;
+  screenH = window.outerHeight;
+  console.log(totalH, screenH);
+}
+
+window.onload = () => {
+  init();
+};
