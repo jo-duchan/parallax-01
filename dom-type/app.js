1db9bf8ae864fdeeeb3b7d14865ee2126b0385f0
jo-duchan <jo_duchan@icloud.com>
2022-12-21 00:15:52 +0900

    Canvas Type Addttion

--- dom-type/app.js
+++ dom-type/app.js
@@ -7,7 +7,7 @@ let screenH;
 
 function init() {
   imageArr = document.querySelectorAll(".layout");
-  console.log(imageArr);
+  // console.log(imageArr);
   totalNum = imageArr.length;
 
   onResize();
@@ -19,7 +19,7 @@ function init() {
 function onScroll() {
   scrollTop = this.scrollY;
   percent = Math.min(Math.max((scrollTop / (totalH - screenH)) * 100, 0), 100);
-
+  console.log(totalH, screenH, percent);
   for (let i = 0; i < totalNum; i++) {
     imageArr[i].style.transform = `perspective(400px) translate3d(0, 0, ${
       scrollTop / (5 * (totalNum - i))
@@ -30,7 +30,7 @@ function onScroll() {
 function onResize() {
   totalH = document.body.offsetHeight;
   screenH = window.outerHeight;
-  console.log(totalH, screenH);
+  // console.log(totalH, screenH, percent);
 }
 
 window.onload = () => {
